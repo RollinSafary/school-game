2f882eae12112025a300008603b6f4542dec9ad0
rollinsafary <rollinsafary@hotmail.com>
2025-03-27 01:29:11 +0400

    electron wip

--- webpack.config.js
+++ webpack.config.js
@@ -5,43 +5,75 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 module.exports = {
   entry: "./src/index.tsx",
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "build"),
     filename: "bundle.js",
+    publicPath: "./",
+    clean: true,
   },
   module: {
     rules: [
       {
         test: /\.(ts|tsx)$/,
         exclude: /node_modules/,
-        use: "ts-loader",
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                "@babel/preset-env",
+                "@babel/preset-react",
+                "@babel/preset-typescript",
+              ],
+            },
+          },
+        ],
       },
       {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif|wav|mp3)$/i,
+        test: /\.(png|svg|jpg|jpeg|gif|wav|mp3|ico|icns)$/i,
         type: "asset/resource",
       },
     ],
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js"],
+    extensions: [".tsx", ".ts", ".js", ".jsx"],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
+      template: "./public/index.html",
+      filename: "index.html",
     }),
     new CopyWebpackPlugin({
-      patterns: [{ from: "public", to: "" }],
+      patterns: [
+        {
+          from: "public",
+          to: ".",
+          globOptions: {
+            ignore: ["**/index.html"],
+          },
+        },
+        {
+          from: "src/assets",
+          to: "assets",
+          noErrorOnMissing: true,
+        },
+      ],
     }),
   ],
   devServer: {
     static: {
-      directory: path.join(__dirname, "dist"),
+      directory: path.join(__dirname, "build"),
     },
     compress: true,
     port: 3000,
     hot: true,
+    historyApiFallback: true,
+  },
+  devtool: "source-map",
+  optimization: {
+    minimize: process.env.NODE_ENV === "production",
   },
 };
